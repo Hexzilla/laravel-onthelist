9e5c08bcb5c36c7d4a84979160646529d80c9b2c
Eric Thomas <bluebird6922@gmail.com>
2022-04-06 10:54:52 +0300

    add user controller

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function index($role)
     {
-        echo("role:" . $role);
-        exit(0);
+        $users = User::where('role', $role)->get();
+        return view("admin.user.$role-list", ['users' => $users]);
     }
 }
