487f1570fed680220b66921a72f9cc6979ca82da
Eric Thomas <reactomoss@gmail.com>
2022-04-14 17:41:54 +0300

    autocomplete address

--- resources/views/vendor/venue/create.blade.php
+++ resources/views/vendor/venue/create.blade.php
@@ -466,5 +466,30 @@
             form.removeClass('was-validated');
         });
     });
+
+    function initMap() {
+        const input = document.getElementById("VenueLocation");
+        const options = {
+            fields: ["formatted_address", "geometry", "name"],
+            strictBounds: false,
+            types: ["establishment"],
+        };
+        const autocomplete = new google.maps.places.Autocomplete(input, options);
+        autocomplete.addListener("place_changed", () => {
+            const place = autocomplete.getPlace();
+            console.log('place', place)
+
+            if (!place.geometry || !place.geometry.location) {
+                // User entered the name of a Place that was not suggested and
+                // pressed the Enter key, or the Place Details request failed.
+                //window.alert("No details available for input: '" + place.name + "'");
+                return;
+            }
+        });
+    }
 </script>
+<script
+    src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_API_KEY')}}&callback=initMap&libraries=places&v=weekly" 
+    async
+></script>
 @endsection
